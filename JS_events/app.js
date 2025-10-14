ba83f6bc5941b62b44ef0419b9b5a607b64c3fab
SameenFatima <sameen.fatima993@gmail.com>
2025-10-15 01:06:09 +0500

    event_function

---
+++ JS_events/app.js
@@ -0,0 +1,29 @@
+//________start events__________________
+//___onclick_______
+function greet(e){
+    document.body.style.background='red'
+
+    alert("welcome back")
+}
+
+
+// function move(e) {
+//     e.src="https://i.ytimg.com/vi/Cpl373FOntY/maxresdefault.jpg"
+    
+// }
+// function moveout(e) {
+//     e.src="https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUyYjY4bGt0aXVvMHcza3hlbmM5YWU2NWt1MHk1NXVjbWhuc3BseGFobSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/wHGzEbVMYcAFEX5Z4o/giphy.gif"
+    
+// }
+
+
+function ready(e){
+    console.log("test");
+e.src="https://i.pinimg.com/originals/db/79/1c/db791c6d62ba7ec5635264f8533152cd.gif"
+
+}
+function runcar(e){
+    console.log("test2");
+    e.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsCc74Ptt4oZoy_xIr8yrQ1abW07DLioKjnQ&s"
+    
+}
